36f1f20ae20f31af570a499e37867c989d9c9a33
Jonathan <mail@jonathanstoye.de>
2017-05-31 11:59:56 +0200

    add tests

---
+++ test/hashtable/HashtableTest.java
@@ -0,0 +1,26 @@
+package hashtable;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Jonathan on 31.05.17.
+ */
+public class HashtableTest {
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void put() throws Exception {
+  }
+
+  @Test
+  public void get() throws Exception {
+  }
+
+  @Test
+  public void remove() throws Exception {
+  }
+
+}
