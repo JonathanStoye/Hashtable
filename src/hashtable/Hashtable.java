b6f0a0ea0a1d2c4ff999084400d42364f8227160
Jonathan <mail@jonathanstoye.de>
2017-05-31 10:24:01 +0200

    start hashtable

---
+++ src/hashtable/Hashtable.java
@@ -0,0 +1,21 @@
+package hashtable;
+
+/**
+ * Created by Jonathan on 31.05.17.
+ */
+public class Hashtable implements hashtable.Map {
+  @Override
+  public Object put(Object key, Object value) {
+    return null;
+  }
+
+  @Override
+  public Object get(Object key) {
+    return null;
+  }
+
+  @Override
+  public Object remove(Object key) {
+    return null;
+  }
+}
