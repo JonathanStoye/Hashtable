48f4351c077e7479e2aff50c18a2f7cc6b23bc7c
Jonathan <mail@jonathanstoye.de>
2017-05-31 12:31:24 +0200

    added test for put

--- test/hashtable/HashtableTest.java
+++ test/hashtable/HashtableTest.java
@@ -3,16 +3,26 @@ package hashtable;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Created by Jonathan on 31.05.17.
- */
+import static junit.framework.TestCase.assertEquals;
+
 public class HashtableTest {
+  private Hashtable table;
+
   @Before
   public void setUp() throws Exception {
+    this.table = new Hashtable(3);
+  }
+
+  @Test
+  public void constructor() {
   }
 
   @Test
   public void put() throws Exception {
+    this.table.put(new Integer(2), "test1");
+    this.table.put(new Integer(3), "test2");
+    this.table.put(new Integer(5), "test3");
+    assertEquals(3, this.table.itemCount);
   }
 
   @Test
